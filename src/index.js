1430b37b8fdbbc828fd122ee1ff2f7b7aeaea43c
Karpov Yan <oficialbalunov@gmail.com>
2021-02-08 07:29:16 +0800

    Creating state newPost

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state.jsx';
+import state from './redux/state.js';
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
---
+++ src/redux/state.js
@@ -0,0 +1,34 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: '10'},
+            {id: 2, message: 'It\'s my first post', likesCount: '23'}
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Yan'},
+            {id: 2, name: 'Vasya'},
+            {id: 3, name: 'Petya'},
+            {id: 4, name: 'Nastya'},
+            {id: 5, name: 'Sasha'},
+            {id: 6, name: 'Kostya'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Yo'},
+            {id: 3, message: 'Kek'}
+        ]
+    }
+}
+
+export let addPost = (userMessage) => {
+    let newPost = {
+        id: 5,
+        message: userMessage,
+        likesCount: 0
+    }
+    state.profilePage.posts.push(newPost)
+}
+
+export default state;
