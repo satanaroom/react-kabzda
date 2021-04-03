f058b3e3e05d8959fc784a61f0eb5aeb09c23088
Karpov Yan <oficialbalunov@gmail.com>
2021-04-03 16:19:24 +0800

    Created class component (lesson 54 00:00)

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,30 +3,32 @@ import s from './Users.module.css'
 import axios from "axios";
 import userImage from '../../assets/img/userImage.png'
 
-let Users = (props) => {
-    const getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items)
-            })
-        }
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
+
     }
-    return (
-        <div>
-            <button onClick={getUsers}>Get Users</button>
+    //Пропы в рендер не приходят!
+    render() {
+        return <div>
             {
-                props.users.map(u => <div key={u.id}>
+                this.props.users.map(u => <div key={u.id}>
                     <div>
                         <div>
-                            <img src={u.photos.small !== null ? u.photos.small : userImage } alt={'Users photo'} className={s.avatar}/>
+                            <img src={u.photos.small !== null ? u.photos.small : userImage} alt={'Users photo'}
+                                 className={s.avatar}/>
                         </div>
                         <div>
                             {u.followed
                                 ? <button onClick={() => {
-                                    props.unfollow(u.id)
+                                    this.props.unfollow(u.id)
                                 }}>unfollow</button>
                                 : <button onClick={() => {
-                                    props.follow(u.id)
+                                    this.props.follow(u.id)
                                 }}>follow</button>}
                         </div>
                     </div>
@@ -43,7 +45,7 @@ let Users = (props) => {
                 </div>)
             }
         </div>
-    )
+    }
 }
 
 export default Users
